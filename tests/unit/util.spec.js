8297f36f19809598d6f9695f4206812fa219fa6e
eheinrich <eheinrichjosties@gmail.com>
2021-02-24 17:26:39 -0800

    Add tests for utilities added

--- tests/unit/util.spec.js
+++ tests/unit/util.spec.js
@@ -1,4 +1,13 @@
-import { decimalRaToSexigesimal, decimalDecToSexigesimal, formatValue, formatField } from '@/util';
+import {
+  decimalRaToSexigesimal,
+  decimalDecToSexigesimal,
+  formatValue,
+  formatField,
+  formatDate,
+  stateToBsClass,
+  stateToIcon,
+  timeFromNow
+} from '@/util';
 
 describe('decimalRaToSexigesimal', () => {
   it('converts positive decimal right ascension to sexagesimal correctly', () => {
@@ -29,5 +38,43 @@ describe('formatField', () => {
     let expected = 'Foo Bar Baz';
     let result = formatField('foo_bar_baz');
     expect(result).toEqual(expected);
+  });
+});
+
+describe('formatDate', () => {
+  it('formats date using default format', () => {
+    let date = '2021-02-03T04:55:28.474845Z';
+    let result = formatDate(date);
+    let expected = '2021-02-03 04:55:28';
+    expect(result).toEqual(expected);
+  });
+  it('formats date using supplied format', () => {
+    let date = '2021-02-03T04:55:28.474845Z';
+    let result = formatDate(date, 'YYYY/MM/DD HH:mm');
+    let expected = '2021/02/03 04:55';
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('stateToBsClass', () => {
+  it('returns correct class for pending', () => {
+    let result = stateToBsClass('PENDING', 'text');
+    let expected = 'text-info';
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('stateToIcon', () => {
+  it('returns correct icon for pending', () => {
+    let expected = 'fa fa-fw fa-sync';
+    let result = stateToIcon('PENDING');
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('timeFromNow', () => {
+  it('returns time in past', () => {
+    let result = timeFromNow('2009-10-10 00:00:00'); // This date is definitely in the past
+    expect(result).toContain('ago');
   });
 });
